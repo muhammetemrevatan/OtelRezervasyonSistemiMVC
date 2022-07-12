3113e06df315d164864d52b6d4e756e6bca9ef54
muhammetemrevatan <emre.vatan98@hotmail.com>
2022-07-12 12:36:43 +0300

    first commit,

---
+++ OtelMvcProje/Controllers/DefaultController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelMvcProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View(); 
+        }
+
+        public ActionResult AnaSayfa()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimda()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSosyalMedya()
+        {
+            return PartialView();
+        }
+    }
+}
